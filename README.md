c9d9a714d46fc327c9497a852fd7e9ccacf82476
traumatram <85923335+traumatram@users.noreply.github.com>
2021-06-18 22:57:44 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# How to install
+- Download [Zipfile](https://github.com/Tehchy/Krunker-Resource-Swapper/archive/master.zip)
+- Extract
+- Add your resources
+- Open `chrome://extensions/` in chrome
+- Drag and drop the this folder into that page
+
+# How to use
+- Resource Swapper automatically looks for any files you put in a folder and swaps them
+- You MUST follow the folder structure of krunker
+- EX: weapons models go in models/weapons/weapon_NUMBER.obj
+	- Sniper Rifle = 1
+	- Assault Rifle = 2
+	- Pistol = 3
+	- Submachine Gun = 4
+	- Revolver = 5
+	- Shotgun = 6
+	- Light Machine Gun = 7
+	- Semi Auto = 8
+	- Rocket Launcher = 9
+	- Akimbo Uzi = 10
+	- Desert Eagle = 11
+	- Alien Blaster = 13
---
+++ init.js
@@ -0,0 +1,32 @@
+let request = chrome.webRequest.onBeforeRequest;
+
+chrome.runtime.getPackageDirectoryEntry((root) => {
+    let reader = root.createReader();
+    reader.readEntries((results) => {
+        searchDir(root, results.filter(x => !['init.js', 'manifest.json', 'README.md', 'LICENSE', '.git'].includes(x.name)));
+    });
+});
+
+function searchDir(parent, directories) {
+    for (let directory of directories) {
+        parent.getDirectory(directory.name, {create: false}, (dir) => {
+            let reader = dir.createReader();
+            reader.readEntries((results) => {
+                let newDirs = results.filter(x => x.isDirectory);
+                let useAssets = !(/\/(css|img|libs|sound)$/.test(dir.fullPath));
+                let files = results.filter(x => x.isFile);
+                if (newDirs.length) searchDir(dir, newDirs);
+                for (let file of files) {
+                    request.addListener((details) => {
+                        return {
+                            redirectUrl:chrome.extension.getURL(file.fullPath.replace('/crxfs/', ''))
+                        }
+                    }, {
+                        urls: ['*://' + (useAssets ? 'assets.':'') + 'krunker.io/' + file.fullPath.replace('/crxfs/', '') + '*']
+                    }, ['blocking']);
+                }
+            });
+        });
+    }
+}
+//checks all folders
